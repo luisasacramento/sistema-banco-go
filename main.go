e480c288b250ec91ca0d6b42d7edcbaf3a4f57db
Luisa Santos <luisacristina.sn@icloud.com>
2023-12-11 22:01:20 -0300

    Tipos Aninhados

--- main.go
+++ main.go
@@ -3,21 +3,18 @@ package main
 import (
 	"fmt"
 	"banco/contas"
+	"banco/clientes"
 
 )
 
 
 func main() {
+	contaDoBruno := contas.ContaCorrente{Titular: clientes.Titular{
+		Nome: "Bruno",
+		CPF: "123.111.123.12",
+		Profissao: "Desenvovedor"},
+		NumeroAgencia: 123, NumeroConta: 12345, Saldo: 100} 
 
-	contaDaLuisa := contas.ContaCorrente{Titular: "Luisa", Saldo: 300}
-	contaDoGuilherme := contas.ContaCorrente{Titular: "Guilherme", Saldo: 100}
-
-	status := contaDaLuisa.Transferir(200, &contaDoGuilherme)
-
-	fmt.Println(status)
-	fmt.Println(contaDaLuisa)
-	fmt.Println(contaDoGuilherme)
-
+	fmt.Println(contaDoBruno)
+	}
 	
-
-}
