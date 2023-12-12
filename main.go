a4902c23872be01d31d98af9a0c0944d17c56094
Luisa Santos <luisacristina.sn@icloud.com>
2023-12-11 23:28:00 -0300

    interface commit

--- main.go
+++ main.go
@@ -6,14 +6,26 @@ import (
 
 )
 
+func PagarBoleto (conta verificarConta, valorDoBoleto float64){
+	conta.Sacar(valorDoBoleto)
+}
 
+type verificarConta interface{
+	Sacar(valor float64) string
+}
 
 func main() {
     contaDoDenis := contas.ContaPoupanca{}
     contaDoDenis.Depositar(100)
-    contaDoDenis.Sacar(55)
+    PagarBoleto(&contaDoDenis, 60)
 
     fmt.Println(contaDoDenis.ObterSaldo())
+
+	contaDaLuisa  := contas.ContaCorrente{}
+	contaDaLuisa.Depositar(500)
+	PagarBoleto(&contaDaLuisa, 400)
+
+	fmt.Println(contaDaLuisa.ObterSaldo())
 }
 	
 	
